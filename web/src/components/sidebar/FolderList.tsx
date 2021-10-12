bebde02019efade4d1d6c68674d5c6b956f5583c
이도현 <ksmfou98@naver.com>
2021-10-12 22:32:58 +0900

    [#4]/fix 드래그 앤 드롭 라이브러리 변경

--- web/src/components/sidebar/FolderList.tsx
+++ web/src/components/sidebar/FolderList.tsx
@@ -1,34 +1,103 @@
-import React, { ReactElement } from 'react';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
-import FolderItem from './FolderItem';
-import FolderSubItem from './FolderSubItem';
-import { IData } from './SideBar';
+import React, { ReactElement, useState } from 'react';
+import Navigation, { AkNavigationItem } from '@atlaskit/navigation';
+import Button from '@atlaskit/button';
+import Tree, {
+  mutateTree,
+  moveItemOnTree,
+  TreeData,
+  TreeSourcePosition,
+  TreeDestinationPosition,
+  ItemId,
+  RenderItemParams,
+  TreeItem,
+} from '@atlaskit/tree';
+import { FaCaretRight, FaCaretDown } from 'react-icons/fa';
+import styled from 'styled-components';
+import atlassianTree from './data/atlassianTreeMock.json';
 
-interface FolderListProps {
-  folders: IData[];
-  onDragEnd: () => void;
-}
+const FolderListWrapper = styled.div``;
 
-function FolderList({ folders, onDragEnd }: FolderListProps): ReactElement {
+function FolderList(): ReactElement {
+  const [tree, setTree] = useState<TreeData>(atlassianTree);
+
+  const getIcon = (
+    item: TreeItem,
+    onExpand: (itemId: ItemId) => void,
+    onCollapse: (itemId: ItemId) => void,
+  ) => {
+    if (item.children && item.children.length > 0) {
+      return item.isExpanded ? (
+        <Button
+          spacing="none"
+          appearance="subtle-link"
+          onClick={() => onCollapse(item.id)}
+        >
+          <FaCaretDown />
+        </Button>
+      ) : (
+        <Button
+          spacing="none"
+          appearance="subtle-link"
+          onClick={() => onExpand(item.id)}
+        >
+          <FaCaretRight />
+        </Button>
+      );
+    }
+    return null;
+  };
+
+  const renderItem = ({
+    item,
+    onExpand,
+    onCollapse,
+    provided,
+    snapshot,
+  }: RenderItemParams) => {
+    return (
+      <div ref={provided.innerRef} {...provided.draggableProps}>
+        <AkNavigationItem
+          isDragging={snapshot.isDragging}
+          text={item.data ? item.data.title : ''}
+          icon={getIcon(item, onExpand, onCollapse)}
+          dnd={{ dragHandleProps: provided.dragHandleProps }}
+        />
+      </div>
+    );
+  };
+
+  const onExpand = (itemId: ItemId) => {
+    setTree(mutateTree(tree, itemId, { isExpanded: true }));
+  };
+
+  const onCollapse = (itemId: ItemId) => {
+    setTree(mutateTree(tree, itemId, { isExpanded: false }));
+  };
+
+  const onDragEnd = (
+    source: TreeSourcePosition,
+    destination?: TreeDestinationPosition,
+  ) => {
+    if (!destination) {
+      return;
+    }
+    const test = moveItemOnTree(tree, source, destination);
+    setTree(test);
+  };
+
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable-list">
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            {folders.map(
-              (folder, index) =>
-                !folder.parentId && (
-                  <div key={index}>
-                    <FolderItem folder={folder} index={index} isParent />
-                    <FolderSubItem folders={folders} parentId={folder.id} />
-                  </div>
-                ),
-            )}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <FolderListWrapper>
+      <Navigation>
+        <Tree
+          tree={tree}
+          renderItem={renderItem}
+          onExpand={onExpand}
+          onCollapse={onCollapse}
+          onDragEnd={onDragEnd}
+          isDragEnabled
+        />
+      </Navigation>
+    </FolderListWrapper>
   );
 }
 
--- web/src/components/sidebar/SideBar.tsx
+++ web/src/components/sidebar/SideBar.tsx
@@ -1,47 +1,6 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import styled from 'styled-components';
-import FolderList from './FolderList';
-
-export interface IData {
-  id: string;
-  name: string;
-  parentId?: string;
-}
-
-const fakeData: IData[] = [
-  {
-    id: '11231d1d',
-    name: '첫번째',
-  },
-  {
-    id: '212d12d12d',
-    name: '첫번째 자식1',
-    parentId: '11231d1d',
-  },
-  {
-    id: '3asd21dasd',
-    name: '첫번째 자식 2',
-    parentId: '11231d1d',
-  },
-  {
-    id: '4123dasd2',
-    name: '첫번째 자식1의 자식 1 ',
-    parentId: '212d12d12d',
-  },
-  {
-    id: '5123sad2',
-    name: '첫번째 자식 3',
-    parentId: '11231d1d',
-  },
-  {
-    id: '5123ssda2aad2',
-    name: '두번째 ',
-  },
-  {
-    id: '5123asd26sad2',
-    name: '세번째 ',
-  },
-];
+import Example from './FolderList';
 
 const SideBarWrapper = styled.div`
   width: 240px;
@@ -50,15 +9,9 @@ const SideBarWrapper = styled.div`
 `;
 
 function SideBar(): ReactElement {
-  const [folders, setFolders] = useState(fakeData);
-
-  const onDragEnd = () => {
-    console.log('drag end');
-  };
-
   return (
     <SideBarWrapper>
-      <FolderList folders={folders} onDragEnd={onDragEnd} />
+      <Example />
     </SideBarWrapper>
   );
 }
