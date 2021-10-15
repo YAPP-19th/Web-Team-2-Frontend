1d5ee757607281bb8a9fa4fcd648def740bb9674
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-10-15 15:07:16 +0900

    [#4]/feat FolderMenu 빈공간 클릭시 꺼지게 설정

--- web/src/components/sidebar/FolderList.tsx
+++ web/src/components/sidebar/FolderList.tsx
@@ -14,7 +14,9 @@ const FolderListWrapper = styled.div`
   position: relative;
   /* overflow: auto; */
 `;
-const FolderItemWrapper = styled.div``;
+const FolderItemWrapper = styled.div`
+  width: 166px;
+`;
 
 const FolderItemBlock = styled.div`
   display: flex;
@@ -81,52 +83,56 @@ function FolderList(): ReactElement {
     provided,
   }: RenderItemParams): ReactElement => {
     return (
-      <FolderItemWrapper
-        ref={provided.innerRef}
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-      >
-        <FolderItemBlock>
-          <FolderLeftBox>
-            <span>
-              <FolderItemIcon
-                item={item}
-                onCollapse={onCollapse}
-                onExpand={onExpand}
-              />
-            </span>
-            {item.children && item.children.length > 0 ? (
-              <span
-                className="title"
-                role="button"
-                tabIndex={0}
-                onKeyDown={() =>
-                  item.isExpanded ? onCollapse(item.id) : onExpand(item.id)
-                }
-                onClick={() =>
-                  item.isExpanded ? onCollapse(item.id) : onExpand(item.id)
-                }
-              >
-                {item.data ? item.data.title : ''}
+      <>
+        <FolderItemWrapper
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <FolderItemBlock>
+            <FolderLeftBox>
+              <span>
+                <FolderItemIcon
+                  item={item}
+                  onCollapse={onCollapse}
+                  onExpand={onExpand}
+                />
               </span>
-            ) : (
-              <span className="title">{item.data ? item.data.title : ''}</span>
-            )}
-          </FolderLeftBox>
-          <FolderRightBox className="right">
-            <FolderETCButton type="button">
-              <PlusIcon />
-            </FolderETCButton>
-            <FolderETCButton
-              type="button"
-              onClick={(e) => onToggleMenu(e, item.id)}
-            >
-              <MoreIcon />
-            </FolderETCButton>
-          </FolderRightBox>
-          {isOpen === item.id && <FolderMenu top={top} left={left} />}
-        </FolderItemBlock>
-      </FolderItemWrapper>
+              {item.children && item.children.length > 0 ? (
+                <span
+                  className="title"
+                  role="button"
+                  tabIndex={0}
+                  onKeyDown={() =>
+                    item.isExpanded ? onCollapse(item.id) : onExpand(item.id)
+                  }
+                  onClick={() =>
+                    item.isExpanded ? onCollapse(item.id) : onExpand(item.id)
+                  }
+                >
+                  {item.data ? item.data.title : ''}
+                </span>
+              ) : (
+                <span className="title">
+                  {item.data ? item.data.title : ''}
+                </span>
+              )}
+            </FolderLeftBox>
+            <FolderRightBox className="right">
+              <FolderETCButton type="button">
+                <PlusIcon />
+              </FolderETCButton>
+              <FolderETCButton
+                type="button"
+                onClick={(e) => onToggleMenu(e, item.id)}
+              >
+                <MoreIcon />
+              </FolderETCButton>
+            </FolderRightBox>
+          </FolderItemBlock>
+        </FolderItemWrapper>
+        {isOpen === item.id && <FolderMenu top={top} left={left} />}
+      </>
     );
   };
 
--- web/src/components/sidebar/FolderMenu.tsx
+++ web/src/components/sidebar/FolderMenu.tsx
@@ -1,4 +1,6 @@
 import React, { ReactElement } from 'react';
+import { useResetRecoilState } from 'recoil';
+import { folderMenuState } from 'recoil/atoms/folderState';
 import styled from 'styled-components';
 
 interface FolderMenuProps {
@@ -6,25 +8,35 @@ interface FolderMenuProps {
   left: number;
 }
 
-const FolderMenuWrapper = styled.div<{ top: number; left: number }>`
+const FolderMenuWrapper = styled.div`
   position: fixed;
-  top: ${(props) => props.top}px;
-  left: ${(props) => props.left + 20}px;
+  top: 0px;
+  left: 0px;
   z-index: 100;
+  width: 100%;
+  height: 100%;
 `;
 
-const MenuInner = styled.div`
+const MenuInner = styled.div<{ top: number; left: number }>`
   background-color: red;
   width: 80px;
   height: 116px;
   border-radius: 4px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
+  position: fixed;
+  z-index: 9999;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left + 20}px;
 `;
 
 function FolderMenu({ top, left }: FolderMenuProps): ReactElement {
+  const onReset = useResetRecoilState(folderMenuState);
+
   return (
-    <FolderMenuWrapper top={top} left={left}>
-      <MenuInner>FolderMenu</MenuInner>
+    <FolderMenuWrapper onClick={onReset}>
+      <MenuInner top={top} left={left} onClick={(e) => e.stopPropagation()}>
+        FolderMenu
+      </MenuInner>
     </FolderMenuWrapper>
   );
 }
