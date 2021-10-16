5838fc99711557467b74f79d2663c8714fca7c2e
이도현 <ksmfou98@naver.com>
2021-10-16 16:40:09 +0900

    [#4]/feat plus 흰색 아이콘 추가

--- web/src/assets/icons/index.ts
+++ web/src/assets/icons/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as ArrowDownIcon } from './Arrow_down.svg';
 export { ReactComponent as ArrowSideIcon } from './Arrow_side.svg';
 export { ReactComponent as MoreIcon } from './More.svg';
 export { ReactComponent as PlusIcon } from './Plus.svg';
+export { ReactComponent as PlusWhiteIcon } from './Plus_white.svg';
