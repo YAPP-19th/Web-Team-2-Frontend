6930d5835d7af378f9f4e8e8946e755d25cd7a28
이도현 <ksmfou98@naver.com>
2021-10-16 10:44:49 +0900

    [#4]/feat Modal 컴포넌트 생성

---
+++ web/src/components/common/Modal.tsx
@@ -0,0 +1,67 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+
+interface ModalProps {
+  width: number;
+  height: number;
+  isModal: boolean;
+  onToggleModal: () => void;
+  children: React.ReactNode;
+}
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+`;
+
+const Inner = styled.div<{ width: number; height: number; isModal: boolean }>`
+  position: absolute;
+  z-index: 9999;
+  background-color: #fff;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+`;
+
+const Background = styled.div`
+  display: block;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  position: absolute;
+  left: 0;
+  top: 0;
+  opacity: 0.6;
+`;
+
+function Modal({
+  width,
+  height,
+  isModal,
+  onToggleModal,
+  children,
+}: ModalProps): ReactElement {
+  return (
+    <ModalWrapper onMouseDown={onToggleModal}>
+      <Inner
+        onMouseDown={(e) => e.stopPropagation()}
+        width={width}
+        height={height}
+        isModal={isModal}
+      >
+        {children}
+      </Inner>
+      <Background />
+    </ModalWrapper>
+  );
+}
+
+export default Modal;
