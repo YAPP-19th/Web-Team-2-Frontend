907fdd2edfdd26522cb19c27bbf288bc50379af9
zigje9 <pjkwprn@gmail.com>
2021-10-29 13:48:30 +0900

    [#34]/feat bell icon atom state

--- web/src/components/header/Info.tsx
+++ web/src/components/header/Info.tsx
@@ -1,5 +1,7 @@
 import { BellIcon, BellNewIcon } from 'assets/icons';
 import React, { ReactElement } from 'react';
+import { useRecoilState } from 'recoil';
+import { bellState } from 'recoil/atoms/bellState';
 import styled from 'styled-components';
 
 const HeaderInfo = styled.div`
@@ -11,18 +13,23 @@ const HeaderInfo = styled.div`
   justify-content: center;
 `;
 
-const BellIconBox = styled.div``;
+const BellIconBox = styled.div`
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const DefaultBellIcon = styled(BellIcon)``;
 
 const NewBellIcon = styled(BellNewIcon)``;
 
 function Info(): ReactElement {
+  const [bellAlarm, setBellAlarm] = useRecoilState<boolean>(bellState);
+
   return (
     <HeaderInfo>
-      <BellIconBox>
-        <NewBellIcon />
-        <DefaultBellIcon />
+      <BellIconBox onClick={() => setBellAlarm(!bellAlarm)}>
+        {bellAlarm ? <NewBellIcon /> : <DefaultBellIcon />}
       </BellIconBox>
     </HeaderInfo>
   );
---
+++ web/src/recoil/atoms/bellState.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const bellState = atom({
+  key: 'bellState',
+  default: false,
+});
