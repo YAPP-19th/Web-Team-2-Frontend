c096ce1c3248e13b98f4947066d6e39f1f44ff24
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-11-01 15:51:56 +0900

    [#46]/feat SimpleButton 컴포넌트 생성

---
+++ web/src/components/common/SimpleButton.tsx
@@ -0,0 +1,69 @@
+import React, { ReactElement } from 'react';
+import styled, { css } from 'styled-components';
+
+interface SimpleButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+  variant: 'primary' | 'secondary';
+  borderColor?: string;
+  width: string;
+  height: string;
+}
+
+interface ButtonStyledProps {
+  variant: 'primary' | 'secondary';
+  borderColor?: string;
+  width: string;
+  height: string;
+}
+const SimpleButtonStyled = styled.button<ButtonStyledProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  border-radius: 8px;
+  line-height: 1.5;
+  font-size: 14px;
+
+  ${(props) =>
+    props.variant === 'primary'
+      ? css`
+          background-color: ${props.theme.color.primary};
+          color: ${props.theme.color.white0};
+          font-weight: 500;
+          border: none;
+        `
+      : css`
+          background-color: ${props.theme.color.white0};
+          color: ${props.theme.color.black1};
+          border: 1px solid ${props.borderColor};
+        `}
+
+  &:disabled {
+    background-color: ${(props) => props.theme.color.border0};
+    color: ${(props) => props.theme.color.white0};
+    line-height: 1.42;
+    border: none;
+  }
+`;
+
+function SimpleButton({
+  label,
+  variant,
+  borderColor,
+  width,
+  height,
+  ...rest
+}: SimpleButtonProps): ReactElement {
+  return (
+    <SimpleButtonStyled
+      variant={variant}
+      borderColor={borderColor}
+      width={width}
+      height={height}
+      {...rest}
+    >
+      {label}
+    </SimpleButtonStyled>
+  );
+}
+
+export default SimpleButton;
