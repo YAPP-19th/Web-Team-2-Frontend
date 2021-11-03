9d0007a93e5d25ce1e9aaf842f83dc145eb45400
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-11-03 13:50:34 +0900

    [#49]/feat SimpleInput

---
+++ web/src/components/common/SimpleInput.tsx
@@ -0,0 +1,26 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+
+interface SimpleInputProps extends React.HTMLAttributes<HTMLInputElement> {
+  width: string;
+  height: string;
+}
+
+const SimpleInputStyled = styled.input`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  border: 1px solid ${(props) => props.theme.color.border0};
+  padding: 5.5px 12px 5.5px 10px;
+  border-radius: 4px;
+  outline: none;
+`;
+
+function SimpleInput({
+  width,
+  height,
+  ...rest
+}: SimpleInputProps): ReactElement {
+  return <SimpleInputStyled {...rest} width={width} height={height} />;
+}
+
+export default SimpleInput;
