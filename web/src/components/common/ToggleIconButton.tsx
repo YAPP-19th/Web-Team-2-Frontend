c07561f807505a0df8b3aa2ed05c397b27213180
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-11-03 10:35:34 +0900

    [#48]/feat ToggleIconButton 템플릿 생성

---
+++ web/src/components/common/ToggleIconButton.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+
+const ToggleIconButtonStyled = styled.button``;
+
+function ToggleIconButton(): ReactElement {
+  return <ToggleIconButtonStyled>ToggleIconButton</ToggleIconButtonStyled>;
+}
+
+export default ToggleIconButton;
