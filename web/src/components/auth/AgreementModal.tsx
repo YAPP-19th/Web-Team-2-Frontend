da4679b0b0b30f96f6d6c9866fd36d736eb51f2b
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-11-12 15:04:28 +0900

    [#7]/feat 이용 약관 동의, 개인정보 수집/이용 동의 내용 추가

--- web/src/components/auth/AgreementModal.tsx
+++ web/src/components/auth/AgreementModal.tsx
@@ -1,6 +1,10 @@
 import React, { ReactElement } from 'react';
 import ModalTemplate from 'components/common/ModalTemplate';
 import styled from 'styled-components';
+import {
+  AGREEMENT_PRIVACY_POLICY,
+  AGREEMENT_TERMS_AND_CONDITIONS,
+} from 'utils/config';
 
 interface AgreementModalProps {
   isModal: boolean;
@@ -12,11 +16,36 @@ const AgreementModalStyled = styled.div`
   padding: 36px 40px;
 `;
 
+const AgreementTitle = styled.h2`
+  font-size: 18px;
+  color: ${(props) => props.theme.color.black};
+  font-weight: 500;
+  margin: 0;
+  margin-bottom: 16px;
+`;
+
+const AgreementContent = styled.div`
+  height: 636px;
+  overflow-y: scroll;
+  font-size: 14px;
+  margin-bottom: 24px;
+`;
+
 function AgreementModal({
   isModal,
   onToggleModal,
   name,
 }: AgreementModalProps): ReactElement {
+  const title =
+    name === 'termsAndConditions'
+      ? '이용 약관 동의'
+      : '개인정보 수집/이용 동의';
+
+  const content =
+    name === 'termsAndConditions'
+      ? AGREEMENT_TERMS_AND_CONDITIONS
+      : AGREEMENT_PRIVACY_POLICY;
+
   return (
     <ModalTemplate
       width="471px"
@@ -24,7 +53,10 @@ function AgreementModal({
       isModal={isModal}
       onToggleModal={onToggleModal}
     >
-      <AgreementModalStyled>{name}</AgreementModalStyled>
+      <AgreementModalStyled>
+        <AgreementTitle>{title}</AgreementTitle>
+        <AgreementContent dangerouslySetInnerHTML={{ __html: content }} />
+      </AgreementModalStyled>
     </ModalTemplate>
   );
 }
