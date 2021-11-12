8993f908346b3daf5c41887726be7592eb71d5e3
zigje9 <pjkwprn@gmail.com>
2021-11-12 14:06:02 +0900

    [#56]/fix lint error

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
 import Footer from 'components/footer';
 import Header from 'components/header';
-
 import React, { ReactElement } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import Routing from 'routes/Routing';
 import styled, { ThemeProvider } from 'styled-components';
 import GlobalStyle from './assets/styles/globalStyle';
 import { theme } from './assets/styles/theme';
-import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './pages/ErrorPage';
 
 const AppLayout = styled.div`
