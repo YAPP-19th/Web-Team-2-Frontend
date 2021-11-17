dbc05e5a3e783613be3e41ae12e47d49d3f4ae2d
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-11-17 14:27:55 +0900

    [#4]/feat 폴더 스크롤바 커스텀

--- web/src/components/sidebar/index.tsx
+++ web/src/components/sidebar/index.tsx
@@ -16,8 +16,22 @@ const SideBarWrapper = styled.div`
 
 const FolderBox = styled.div`
   max-height: 541px;
-  overflow: scroll;
+  overflow: hidden auto; // hidden auto => overflow가 없을땐 안보이고 있을땐 스크롤
+  overflow-x: auto;
   margin-bottom: 24px;
+  &::-webkit-scrollbar {
+    width: 10px; // 세로 스크롤바 가로 길이
+    height: 10px; // 가로 스크롤바 세로 길이
+  }
+  &::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.color.white};
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme.color.grayLight};
+    border-radius: 10px;
+    background-clip: padding-box;
+    border: 1px solid transparent;
+  }
 `;
 
 function SideBar(): ReactElement {
