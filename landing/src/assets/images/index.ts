ceed3a610983cffcd04de76acd02240e5976636d
이도현 <ksmfou98@naver.com>
2021-11-18 22:38:12 +0900

    [#6]/feat Footer UI

--- landing/src/assets/images/index.ts
+++ landing/src/assets/images/index.ts
@@ -1,5 +1,6 @@
 export { default as S1MainIMG } from "./S1MainIMG.png";
 export { default as LogoIMG } from "./LogoIMG.png";
+export { default as LogoWhite } from "./LogoWhite.png";
 export { default as S2MainIMG } from "./S2MainIMG.png";
 export { default as S3MainIMG } from "./S3MainIMG.png";
 export { default as S4MainIMG } from "./S4MainIMG.png";
--- landing/src/components/footer/index.tsx
+++ landing/src/components/footer/index.tsx
@@ -1,7 +1,39 @@
+import { LogoWhite } from "assets/images";
 import React, { ReactElement } from "react";
+import styled from "styled-components";
+
+const FooterWrapper = styled.footer`
+  width: 100%;
+  height: 280px;
+  background-color: #48bf91;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FooterLogo = styled.img`
+  width: 273px;
+  margin-right: 44px;
+`;
+
+const DownloadButton = styled.button`
+  width: 174px;
+  height: 3.463rem;
+  border: solid 2px #fff;
+  background-color: rgba(72, 191, 145, 0.95);
+  border-radius: 2.2rem;
+  color: #fff;
+  font-size: 1.256rem;
+  font-weight: bold;
+`;
 
 function Footer(): ReactElement {
-  return <footer>footer</footer>;
+  return (
+    <FooterWrapper>
+      <FooterLogo src={LogoWhite} />
+      <DownloadButton>Download {"->"}</DownloadButton>
+    </FooterWrapper>
+  );
 }
 
 export default Footer;
