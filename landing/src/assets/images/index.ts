3c6a6ee87f2599891433d3665dd1b77268990182
이도현 <ksmfou98@naver.com>
2021-11-18 22:21:43 +0900

    [#6]/feat Section4 UI

--- landing/src/assets/images/index.ts
+++ landing/src/assets/images/index.ts
@@ -2,3 +2,4 @@ export { default as S1MainIMG } from "./S1MainIMG.png";
 export { default as LogoIMG } from "./LogoIMG.png";
 export { default as S2MainIMG } from "./S2MainIMG.png";
 export { default as S3MainIMG } from "./S3MainIMG.png";
+export { default as S4MainIMG } from "./S4MainIMG.png";
--- landing/src/components/section4/index.tsx
+++ landing/src/components/section4/index.tsx
@@ -1,7 +1,80 @@
+import { S4MainIMG } from "assets/images";
 import React, { ReactElement } from "react";
+import styled from "styled-components";
+
+const Section4Wrapper = styled.div`
+  width: 100%;
+  height: 762px;
+  background-color: #fff;
+`;
+
+const Section4Inner = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  padding: 185px 0 196px 285px;
+  display: flex;
+`;
+
+const MainImgBox = styled.img`
+  width: 353px;
+  height: 382px;
+  margin-right: 241px;
+`;
+
+const ContentBox = styled.div`
+  padding-top: 44px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+const Title = styled.span`
+  font-family: Cafe24Ssurround;
+  font-size: 38px;
+  color: #000;
+  font-weight: bold;
+  z-index: 10;
+  margin-bottom: 24px;
+`;
+
+const Circle = styled.div`
+  width: 146px;
+  height: 48px;
+  background-color: rgba(72, 191, 145, 0.3);
+  border-radius: 50%;
+  position: absolute;
+  top: 84px;
+  z-index: 1;
+  left: -3px;
+`;
+
+const Text = styled.div`
+  color: #000;
+  font-size: 16px;
+  line-height: 23px;
+`;
 
 function Section4(): ReactElement {
-  return <div>Section4</div>;
+  return (
+    <Section4Wrapper>
+      <Section4Inner>
+        <MainImgBox src={S4MainIMG} />
+
+        <ContentBox>
+          <Title>
+            중요한 북마크는 <br />
+            리마인드함
+          </Title>
+          <Circle />
+
+          <Text>
+            다시 읽고 싶은 정보는 리마인드 알림을 받아요. <br />
+            놓친 알림도 따로 보관해드려요.
+          </Text>
+        </ContentBox>
+      </Section4Inner>
+    </Section4Wrapper>
+  );
 }
 
 export default Section4;
