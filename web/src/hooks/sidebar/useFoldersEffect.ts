b154eb7369d16865c5f9ec90fad95cb1c5451e36
이도현 <ksmfou98@naver.com>
2021-11-19 23:39:38 +0900

    [#4]/refactor useFoldersEffect

--- web/src/hooks/sidebar/useFoldersEffect.ts
+++ web/src/hooks/sidebar/useFoldersEffect.ts
@@ -1,15 +1,12 @@
-import { TreeData } from '@atlaskit/tree';
 import tempData from 'components/sidebar/data/atlassianTreeMock.json';
 import { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { folderState } from 'recoil/atoms/folderState';
 
-export default function useFoldersEffect(): { folders: TreeData } {
-  const [folders, setFolders] = useRecoilState(folderState);
+export default function useFoldersEffect(): void {
+  const setFolders = useSetRecoilState(folderState);
   useEffect(() => {
     // 현재는 임시데이터 사용 / 나중에 api 생기면 asnyc await으로 데이터 처리
     setFolders(tempData);
   }, []);
-
-  return { folders };
 }
