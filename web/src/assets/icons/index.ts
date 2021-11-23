aa2c2028f09e04a47c9fd10f8026a11da6950263
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-11-23 16:10:25 +0900

    [#41]/feat 푸터 url 추가

--- web/src/assets/icons/index.ts
+++ web/src/assets/icons/index.ts
@@ -18,6 +18,7 @@ export { ReactComponent as More24Icon } from './Icons_More_24.svg';
 export { ReactComponent as Next24Icon } from './Icons_Next_24.svg';
 export { ReactComponent as Search24Icon } from './Icons_Search_24.svg';
 export { ReactComponent as Symbol36Icon } from './Icons_Symbol_36.svg';
+export { ReactComponent as SymbolGray22Icon } from './Icons_Symbol_Gray_22.svg';
 export { ReactComponent as ToggleOffIcon } from './Icons_Toggle_off.svg';
 export { ReactComponent as ToggleOnIcon } from './Icons_Toggle_on.svg';
 export { ReactComponent as SelectedTrashIcon } from './Icons_trash_selected.svg';
