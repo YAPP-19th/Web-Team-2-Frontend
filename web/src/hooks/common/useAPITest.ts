5d5d5213119ed0320045c89ef999023257947fab
이도현 <ksmfou98@naver.com>
2021-12-01 22:02:27 +0900

    [#61]/feat 완료 데이터 확인

--- web/src/hooks/common/useAPITest.ts
+++ web/src/hooks/common/useAPITest.ts
@@ -6,11 +6,11 @@ import { useEffect } from 'react';
 
 const BASE_URL = 'http://3.38.152.22:8081/api/v1';
 const token =
-  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIzIiwiaWF0IjoxNjM4MjU4MDQ2LCJleHAiOjE2Njk3OTQwNDZ9.PthU5QA6sU7pKDi_11YUIA7srSavb7xFKJ8ST75PB4_i4xURAt-ojov3EfoxnjYQtmNNfjzDVoKY37FyMdW5wA';
+  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjM4MzYzMzMzLCJleHAiOjE2Mzg0NDk3MzN9.8kfraj8oe37OdHMd5CRPJCV8rOLzNaLQzAs7Cvx7BtEuX_gFS1Z2WvkWsNRIYMLuksNoIL50fq50gJSCDihP-g';
 
 const header = {
   headers: {
-    Authorization: `Bearer ${token}`,
+    AccessToken: `Bearer ${token}`,
   },
 };
 
@@ -40,13 +40,12 @@ export default function useAPITest() {
     return response.data;
   };
 
-  // 로그인 & 회원가입
+  // 로그인 & 회원가입 (완료)
   const googleLogin = async () => {
     const body = {
-      email: 'test123@gmail.com',
-      imageUrl:
-        'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-      name: 'test123',
+      email: 'abc@gmail.com',
+      imageUrl: 'http://www.asdkljzxcoasdkas.com/',
+      name: 'juhyun',
       socialType: 'google',
     };
     const response = await axios.post(`${BASE_URL}/user/oauth2Login`, body);
@@ -59,10 +58,10 @@ export default function useAPITest() {
     return response.data;
   };
 
-  // 검색 // 문서에 header 넣으라고 안되어 있는데 안넣는게 맞나?
+  // 검색 (완료)
   const search = async () => {
-    const userId = 'test123';
-    const keyWord = '아이유';
+    const userId = 1;
+    const keyWord = '네이버';
     const page = 0; // 몇번째 페이지
     const size = 12; // 한페이지당 보여줄 갯수
     const sort:
@@ -72,14 +71,15 @@ export default function useAPITest() {
       | 'clickCount,asc' = 'saveTime,desc';
     const remind = true;
     const response = await axios.get(
-      `${BASE_URL}/${userId}/${keyWord}?page=${page}&size=${size}&sort=${sort}&remind=${remind}`,
+      `${BASE_URL}/bookmark/${userId}/${keyWord}?page=${page}&size=${size}&sort=${sort}&remind=${remind}`,
+      header,
     );
     return response.data;
   };
 
-  // 북마크 조회(페이징)
+  // 북마크 조회(페이징) (완료)
   const getBookmarks = async () => {
-    const folderId = '123123123';
+    const folderId = '1';
     const page = 0; // 몇번째 페이지
     const size = 9; // 한페이지당 보여줄 갯수
     const sort:
@@ -89,7 +89,7 @@ export default function useAPITest() {
       | 'clickCount,asc' = 'saveTime,desc';
     const remind = true;
     const response = await axios.get(
-      `${BASE_URL}/${folderId}?page=${page}&size=${size}&sort=${sort}&remind=${remind}`,
+      `${BASE_URL}/bookmark/${folderId}?page=${page}&size=${size}&sort=${sort}&remind=${remind}`,
       header,
     );
     return response.data;
@@ -140,7 +140,7 @@ export default function useAPITest() {
     const body = {
       parentId: 0,
       name: '첫번째 보관함',
-      index: 0,
+      index: 2,
     };
     const response = await axios.post(`${BASE_URL}/folder`, body, header);
     return response.data;
@@ -262,7 +262,7 @@ export default function useAPITest() {
       //   const response = await googleLogin();
       //   const response = await reIssuanceAccessToken();
       //   const response = await search();
-      //   const response = await getBookmarks();
+      const response = await getBookmarks();
       //   const response = await getTrashBookmark();
       //   const response = await restoreTrash();
       //   const response = await deleteTrash();
@@ -276,7 +276,7 @@ export default function useAPITest() {
       //   const response = await updateBookmark();
       //   const response = await moveBookmark();
       //   const response = await deleteBookmark();
-      //   console.log(response);
+      console.log(response);
     };
     test();
   }, []);
