d5cc241992a7b7fd0372c77e17ee40341017996f
zigje9 <pjkwprn@gmail.com>
2021-12-04 19:44:30 +0900

    [#62]/feat add logout function

--- web/src/utils/auth.ts
+++ web/src/utils/auth.ts
@@ -1,2 +1,9 @@
+import Path from 'routes/path';
+
 export const isLogin = (): boolean =>
   Boolean(localStorage.getItem('accessToken'));
+
+export const logout = (): void => {
+  localStorage.clear();
+  window.location.href = Path.LoginPage;
+};
