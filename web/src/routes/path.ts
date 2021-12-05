602cb156d2ad9b8ba0d057cc32b2589737ebf359
이도현 <ksmfou98@naver.com>
2021-12-05 13:51:08 +0900

    [#43]/feat Path 추가

--- web/src/routes/path.ts
+++ web/src/routes/path.ts
@@ -1,11 +1,13 @@
 enum Path {
   Home = '/*',
-  MainPage = '/main',
+  MainPage = '/main/*',
   MyPage = '/mypage',
   LoginPage = '/login',
   RegisterPage = '/register',
   ResetPasswordPage = '/resetpw',
   SendPasswordPage = '/sendpw',
+  Trash = '/trash',
+  Search = '/search/*',
 }
 
 export default Path;
