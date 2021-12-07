5c9371f654c6bd6f0ead0909cd0498bbf4216132
이도현 <ksmfou98@naver.com>
2021-12-07 21:01:00 +0900

    [#66]/fix eslint error 수정

--- web/src/hooks/bookmark/useBookmarksEffect.ts
+++ web/src/hooks/bookmark/useBookmarksEffect.ts
@@ -1,21 +1,21 @@
-import { getBookmarks } from 'api/bookmarkAPI';
+// import { getBookmarks } from 'api/bookmarkAPI';
 import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { bookmarksState } from 'recoil/atoms/bookmarkState';
+// import { useSetRecoilState } from 'recoil';
+// import { bookmarksState } from 'recoil/atoms/bookmarkState';
 
 export default function useBookmarksEffect(path: string): void {
-  const setBookmarks = useSetRecoilState(bookmarksState);
+  // const setBookmarks = useSetRecoilState(bookmarksState);
 
   useEffect(() => {
     // path === "main"  이면 전체 도토리
     // path === "trash" 이면 휴지통
     // path === "123123" 이면 특정 폴더
-    // async function temp() {
-    //   // eslint-disable-next-line no-console
-    //   console.log(path);
-    //   const data = await getBookmarks();
-    //   setBookmarks(data.content);
-    // }
-    // if (path) temp();
+    async function temp() {
+      // eslint-disable-next-line no-console
+      console.log(path);
+      // const data = await getBookmarks();
+      // setBookmarks(data.content);
+    }
+    if (path) temp();
   }, [path]);
 }
