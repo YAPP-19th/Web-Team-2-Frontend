8224215c8a4d99dc6512732b22550dd581f70154
zigje9 <pjkwprn@gmail.com>
2021-12-08 16:16:21 +0900

    [#69]/feat add searchAPI in bookamrkAPI

--- web/src/api/bookmarkAPI.ts
+++ web/src/api/bookmarkAPI.ts
@@ -20,6 +20,26 @@ export const getTrashBookmark = (
 };
 
 /**
+ * @param {string} keyword 검색내용
+ * @param {number} page 몇번째 페이지
+ * @param {number} size 페이지당 아이템 개수
+ * @param {string} sort 필터링
+ * @param {boolean} remind 리마인드 여부
+ * @returns Promise<bookmarks.IBookmarkGetResponse>
+ */
+export const getSearchBookmark = (
+  keyword: string,
+  page: number,
+  size: number,
+  sort: string,
+  remind: boolean,
+): Promise<bookmarks.IBookmarkGetResponse> => {
+  return Client.getAxios<bookmarks.IBookmarkGetResponse>(
+    `api/v1/bookmark/search/${keyword}?page=${page}&size=${size}&sort=${sort}&remind=${remind}`,
+  );
+};
+
+/**
  * @param {number} page 몇번째 페이지
  * @param {number} size 페이지당 아이템 개수
  * @param {string} sort 필터링
