2931b9ba266b90f2623bebbed32211f728908d25
이도현 <ksmfou98@naver.com>
2021-12-08 21:49:40 +0900

    [#71]/feat MyPageTemplate UI

--- web/src/pages/MyPage.tsx
+++ web/src/pages/MyPage.tsx
@@ -1,31 +1,20 @@
 import AccountSetting from 'components/mypage/AccountSetting';
 import Configuration from 'components/mypage/Configuration';
+import MyPageTemplate from 'components/mypage/MyPageTemplate';
 import Profile from 'components/mypage/Profile';
 import React, { ReactElement } from 'react';
 import { useRecoilValue } from 'recoil';
 import { userState } from 'recoil/atoms/userState';
-import styled from 'styled-components';
-
-const MyPageWrapper = styled.div`
-  width: 667px;
-  margin: 0 auto;
-`;
-
-const MyPageInner = styled.div`
-  padding-top: 48px;
-`;
 
 function MyPage(): ReactElement {
   const user = useRecoilValue(userState);
 
   return (
-    <MyPageWrapper>
-      <MyPageInner>
-        <Profile user={user} />
-        <Configuration />
-        <AccountSetting email={user.email} />
-      </MyPageInner>
-    </MyPageWrapper>
+    <MyPageTemplate>
+      <Profile user={user} />
+      <Configuration />
+      <AccountSetting email={user.email} />
+    </MyPageTemplate>
   );
 }
 
