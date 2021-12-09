36bd7800e7bfcd3d93554411974746966f9a619a
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-12-09 10:45:34 +0900

    [#71]/feat 프로필 이미지 업로드 UI

--- web/src/components/mypage/ProfileEditForm.tsx
+++ web/src/components/mypage/ProfileEditForm.tsx
@@ -1,3 +1,5 @@
+import { X16BigIcon } from 'assets/icons';
+import SimpleButton from 'components/common/SimpleButton';
 import SmallBlackLabel from 'components/common/SmallBlackLabel';
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
@@ -9,6 +11,50 @@ const ProfileEditFormWrapper = styled.div`
 
 const ImgFormRow = styled.div`
   margin-bottom: 45px;
+  display: flex;
+`;
+
+const FormLabel = styled(SmallBlackLabel)`
+  padding-top: 20px;
+`;
+
+const ProfileImageBox = styled.div`
+  width: 72px;
+  height: 72px;
+  margin-right: 27px;
+`;
+
+const ProfileImage = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 40px;
+  background-color: ${(props) => props.theme.color.grayLight};
+`;
+
+const UploadContent = styled.div``;
+
+const UploadRow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  color: ${(props) => props.theme.color.grayDark};
+  font-weight: 300;
+  &:first-child {
+    margin-bottom: 8px;
+    padding-top: 14px;
+  }
+  &:last-child {
+    font-size: 12px;
+  }
+`;
+
+const UploadButton = styled(SimpleButton)`
+  margin-right: 16px;
+`;
+
+const UploadPath = styled.span`
+  line-height: 1.5;
+  margin-right: 4px;
 `;
 
 const NicknameFormRow = styled.div``;
@@ -17,7 +63,24 @@ function ProfileEditForm(): ReactElement {
   return (
     <ProfileEditFormWrapper>
       <ImgFormRow>
-        <SmallBlackLabel width="297px" label="프로필 이미지" />
+        <FormLabel width="297px" label="프로필 이미지" />
+        <ProfileImageBox>
+          <ProfileImage src="https://via.placeholder.com/72x72" />
+        </ProfileImageBox>
+
+        <UploadContent>
+          <UploadRow>
+            <UploadButton
+              label="파일 선택"
+              variant="secondary"
+              width="75px"
+              height="31px"
+            />
+            <UploadPath>선택된 파일 없음</UploadPath>
+            <X16BigIcon />
+          </UploadRow>
+          <UploadRow>최대 10MB의 이미지 파일</UploadRow>
+        </UploadContent>
       </ImgFormRow>
 
       <NicknameFormRow>
