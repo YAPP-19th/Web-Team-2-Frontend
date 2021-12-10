2a45109894281d2feec4794b97d0b3b0d4e2ad1c
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-12-10 21:47:34 +0900

    [#14]/fix https 도메인 수정

--- web/src/api/http.ts
+++ web/src/api/http.ts
@@ -4,7 +4,7 @@ import axios, { AxiosInstance } from 'axios';
 import qs from 'qs';
 import { getTokens, setTokens } from 'utils/auth';
 
-export const BASE_URL = 'http://3.38.152.22:8081';
+export const BASE_URL = 'https://dotoriham.duckdns.org';
 
 class HTTP {
   axios: AxiosInstance;
