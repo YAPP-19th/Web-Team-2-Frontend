163afcd1ecdb1c62e223daf819bd1694b966095b
이도현 <ksmfou98@naver.com>
2021-12-10 23:51:37 +0900

    [#67]/fix rename Loading Component

--- web/src/components/common/Loading.tsx
+++ web/src/components/common/Loading.tsx
@@ -2,18 +2,18 @@ import React, { ReactElement } from 'react';
 import styled from 'styled-components';
 import loadingGIF from 'assets/images/loading.gif';
 
-const LoadingStyled = styled.div`
+const LoadingSpinnerStyled = styled.div`
   position: fixed;
   z-index: 9998;
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
-  background-color: #000;
+  background-color: ${(props) => props.theme.color.black};
   opacity: 0.4;
 `;
 
-const LoadingICON = styled.div`
+const LoadingSpinnerICON = styled.div`
   position: fixed;
   z-index: 10000;
   top: 50%;
@@ -25,13 +25,13 @@ const LoadingICON = styled.div`
   background-size: 150px 150px;
 `;
 
-function Loading(): ReactElement {
+function LoadingSpinner(): ReactElement {
   return (
     <>
-      <LoadingStyled />
-      <LoadingICON />
+      <LoadingSpinnerStyled />
+      <LoadingSpinnerICON />
     </>
   );
 }
 
-export default Loading;
+export default LoadingSpinner;
