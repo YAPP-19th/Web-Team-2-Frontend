b9b4e45636c0535b350132f1bd398d4c9bd7c103
이도현 <ksmfou98@naver.com>
2021-12-17 10:51:36 +0900

    [#77]/fix user인증 isLogin으로 변경

--- web/src/hooks/auth/useLoggedInUserReplace.ts
+++ web/src/hooks/auth/useLoggedInUserReplace.ts
@@ -1,16 +1,14 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
-import { userState } from 'recoil/atoms/userState';
 import Path from 'routes/path';
+import { isLogin } from 'utils/auth';
 
 export default function useLoggedInUserReplace(): void {
-  const user = useRecoilValue(userState);
   const { pathname } = useLocation();
   const { LoginPage, RegisterPage, SendPasswordPage, ResetPasswordPage } = Path;
   const navigate = useNavigate();
   useEffect(() => {
-    if (user.name) {
+    if (isLogin()) {
       if (
         pathname === LoginPage ||
         pathname === RegisterPage ||
