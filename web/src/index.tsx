e79c6fdb743d38cde04eea439d55ce78d38f0ee0
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-12-21 16:09:48 +0900

    [#85]/feat og title, og image 설정

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,7 @@
 import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { getInitialUserInfo } from 'recoil/atoms/initializer';
@@ -9,11 +10,20 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot initializeState={getInitialUserInfo}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </RecoilRoot>
+    <HelmetProvider>
+      <Helmet>
+        <title>도토리함 | 기억하고 공유하는 북마크 보관함</title>
+        <meta
+          name="description"
+          content="흩어진 북마크를 보관하고 공유하고 알림을 받아보세요"
+        />
+      </Helmet>
+      <RecoilRoot initializeState={getInitialUserInfo}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RecoilRoot>
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
