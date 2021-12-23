9d6b9d835e7213e1c766c444646a695c3e1f0adc
zigje9 <pjkwprn@gmail.com>
2021-12-23 16:58:33 +0900

    [#86]/fix firebase service worker config option

--- web/public/firebase-messaging-sw.mjs
+++ web/public/firebase-messaging-sw.mjs
@@ -6,12 +6,9 @@ import { getMessaging } from 'firebase/messaging/sw';
 // https://firebase.google.com/docs/web/setup#config-object
 const firebaseApp = initializeApp({
   apiKey: 'AIzaSyBQ2hhmKBy2S00dMIl1XTnGQbzKasSbVwY',
-  authDomain: 'dotoriham-dfee3.firebaseapp.com',
   projectId: 'dotoriham-dfee3',
-  storageBucket: 'dotoriham-dfee3.appspot.com',
   messagingSenderId: '1055811077974',
   appId: '1:1055811077974:web:08286bc1990346256cb8f7',
-  measurementId: 'G-BZE4CWPKM3',
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
