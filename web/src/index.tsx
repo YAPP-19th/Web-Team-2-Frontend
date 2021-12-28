6c46ec6630aca9f7cbc8b84c2cc842c727715315
이도현 <ksmfou98@naver.com>
2021-12-28 22:03:24 +0900

    [#85]/fix remove helmet 라이브러리

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,6 @@
 import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { getInitialUserInfo } from 'recoil/atoms/initializer';
@@ -10,32 +9,11 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <Helmet>
-        <title>도토리함 | 기억하고 공유하는 북마크 보관함</title>
-        <meta
-          name="description"
-          content="흩어진 북마크를 보관하고 공유하고 알림을 받아보세요"
-        />
-        <meta
-          property="og:title"
-          content="도토리함 | 기억하고 공유하는 북마크 보관함"
-        />
-        <meta
-          property="og:description"
-          content="흩어진 북마크를 보관하고 공유하고 알림을 받아보세요"
-        />
-        <meta
-          property="og:image"
-          content="https://i.ibb.co/t8sxXnv/og-image.png"
-        />
-      </Helmet>
-      <RecoilRoot initializeState={getInitialUserInfo}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </RecoilRoot>
-    </HelmetProvider>
+    <RecoilRoot initializeState={getInitialUserInfo}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
 );
