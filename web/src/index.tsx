0b84ded77b5b41ec67950c0b68d28f4dfdb0664d
이도현 <ksmfou98@naver.com>
2021-12-28 21:51:53 +0900

    [#85]/feat og image

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -17,6 +17,18 @@ ReactDOM.render(
           name="description"
           content="흩어진 북마크를 보관하고 공유하고 알림을 받아보세요"
         />
+        <meta
+          property="og:title"
+          content="도토리함 | 기억하고 공유하는 북마크 보관함"
+        />
+        <meta
+          property="og:description"
+          content="흩어진 북마크를 보관하고 공유하고 알림을 받아보세요"
+        />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/t8sxXnv/og-image.png"
+        />
       </Helmet>
       <RecoilRoot initializeState={getInitialUserInfo}>
         <BrowserRouter>
